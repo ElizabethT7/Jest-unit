e4542a84c443c41de71eb069cb5999a524e5db69
ElizabethT7 <tymofieievai@gmail.com>
2025-02-19 13:43:07 +0300

    feat: add some tests

--- src/validateValue/validateValue.test.js
+++ src/validateValue/validateValue.test.js
@@ -1,5 +1,25 @@
 const validateValue = require("./validateValue");
 
-test("Валидация значения", () => {
+/*test("Валидация значения", () => {
   expect(validateValue(50)).toBe(true);
+});*/
+
+//если несколько тестов, то оборачиваем все в
+
+describe("validateValue", () => {
+  test("Коректное значение", () => {
+    expect(validateValue(50)).toBe(true);
+  });
+  test("Меньше корректного", () => {
+    expect(validateValue(-1)).toBe(false);
+  });
+  test("Больше корректного", () => {
+    expect(validateValue(101)).toBe(false);
+  });
+  test("Граничное значение снизу", () => {
+    expect(validateValue(0)).toBe(true);
+  });
+  test("Граничное значение сверху", () => {
+    expect(validateValue(100)).toBe(true);
+  });
 });
